f83c12c54e9e32bf8da61525024ad894de09c11a
Anthony Barone <anthony.barone@dealer.com>
2014-10-09 22:41:05 -0400

    Updated selenium to 2.43.1

--- tasks/selenium_webdriver.js
+++ tasks/selenium_webdriver.js
@@ -23,7 +23,7 @@ var spawn = require('child_process').spawn,
     phantomProcess = null,
     fs = require('fs' ),
     os = require('os' ),
-    JAR_NAME = 'selenium-server-standalone-2.42.2.jar';
+    JAR_NAME = 'selenium-server-standalone-2.43.1.jar';
 
 function getEnv () {
     return 'Environment: [' + os.type() + ',' +
